c5f0b46d426264f452e077619e243b43b9c8a60f
Erdem Ozgen <m.erdemozgen@gmail.com>
2022-03-02 08:51:32 +0300

    add model package

--- main.go
+++ main.go
@@ -2,26 +2,13 @@ package main
 
 import (
 	"fmt"
+	"homework-2-ErdemOzgen/model"
 	"strings"
 )
 
 //------------------STRUCTS------------------
 // Arrange field in structs for good memory arrangement
-type Book struct {
-	Author             // Author Struct Embedded
-	ID          int64  // Id starts from 0 to increment by 1
-	BookName    string // Book Name
-	ISBN        string // Book ISBN
-	PageNumber  int64  //
-	Price       int64  // TODO: change price to float64
-	StockAmount int64  //
-	IsDelete    bool   // Check for book deleted or not
-
-}
-type Author struct {
-	Name       string
-	AuthorInfo string
-}
+type Book model.Book
 
 //----------------ENDSTRUCTS----------------
 //------------------FUNCTIONS AND Methods------------------
