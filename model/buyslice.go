224f9162b46b4442c348afa9716371b46b49c709
ErdemOzgen <m.erdemozgen@gmail.com>
2022-03-03 23:33:35 +0300

    add buyslice

--- model/buyslice.go
+++ model/buyslice.go
@@ -2,7 +2,7 @@ package model
 
 import "fmt"
 
-func Buy(id, amount int, books []Book) {
+func BuySlice(id, amount int, books []Book) {
 	books[IDtoIndex(id)].BuyBook(amount) // TODO: check error if  error return DONOT print
 	fmt.Println("Book Bought")           // TODO	: Change to error
 }
